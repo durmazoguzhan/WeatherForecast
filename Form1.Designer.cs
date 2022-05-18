6da14b28f40bf57a906b4b48c41d388b614c6e22
durmazoguzhan <durmazoguzhan@yahoo.com>
2022-05-18 23:28:59 +0300

    first commit - created methods that list cities by country with api

---
+++ Form1.Designer.cs
@@ -0,0 +1,317 @@
+﻿
+namespace WeatherForecast
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.cboxCountry = new System.Windows.Forms.ComboBox();
+            this.cboxCity = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // cboxCountry
+            // 
+            this.cboxCountry.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboxCountry.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cboxCountry.FormattingEnabled = true;
+            this.cboxCountry.Items.AddRange(new object[] {
+            "Afghanistan",
+            "Albania",
+            "Algeria",
+            "Andorra",
+            "Angola",
+            "Antigua & Deps",
+            "Argentina",
+            "Armenia",
+            "Australia",
+            "Austria",
+            "Azerbaijan",
+            "Bahamas",
+            "Bahrain",
+            "Bangladesh",
+            "Barbados",
+            "Belarus",
+            "Belgium",
+            "Belize",
+            "Benin",
+            "Bhutan",
+            "Bolivia",
+            "Bosnia Herzegovina",
+            "Botswana",
+            "Brazil",
+            "Brunei",
+            "Bulgaria",
+            "Burkina",
+            "Burundi",
+            "Cambodia",
+            "Cameroon",
+            "Canada",
+            "Cape Verde",
+            "Central African Rep",
+            "Chad",
+            "Chile",
+            "China",
+            "Colombia",
+            "Comoros",
+            "Congo",
+            "Congo {Democratic Rep}",
+            "Costa Rica",
+            "Croatia",
+            "Cuba",
+            "Cyprus",
+            "Czech Republic",
+            "Denmark",
+            "Djibouti",
+            "Dominica",
+            "Dominican Republic",
+            "East Timor",
+            "Ecuador",
+            "Egypt",
+            "El Salvador",
+            "Equatorial Guinea",
+            "Eritrea",
+            "Estonia",
+            "Ethiopia",
+            "Fiji",
+            "Finland",
+            "France",
+            "Gabon",
+            "Gambia",
+            "Georgia",
+            "Germany",
+            "Ghana",
+            "Greece",
+            "Grenada",
+            "Guatemala",
+            "Guinea",
+            "Guinea-Bissau",
+            "Guyana",
+            "Haiti",
+            "Honduras",
+            "Hungary",
+            "Iceland",
+            "India",
+            "Indonesia",
+            "Iran",
+            "Iraq",
+            "Ireland {Republic}",
+            "Israel",
+            "Italy",
+            "Ivory Coast",
+            "Jamaica",
+            "Japan",
+            "Jordan",
+            "Kazakhstan",
+            "Kenya",
+            "Kiribati",
+            "Korea North",
+            "Korea South",
+            "Kosovo",
+            "Kuwait",
+            "Kyrgyzstan",
+            "Laos",
+            "Latvia",
+            "Lebanon",
+            "Lesotho",
+            "Liberia",
+            "Libya",
+            "Liechtenstein",
+            "Lithuania",
+            "Luxembourg",
+            "Macedonia",
+            "Madagascar",
+            "Malawi",
+            "Malaysia",
+            "Maldives",
+            "Mali",
+            "Malta",
+            "Marshall Islands",
+            "Mauritania",
+            "Mauritius",
+            "Mexico",
+            "Micronesia",
+            "Moldova",
+            "Monaco",
+            "Mongolia",
+            "Montenegro",
+            "Morocco",
+            "Mozambique",
+            "Myanmar, {Burma}",
+            "Namibia",
+            "Nauru",
+            "Nepal",
+            "Netherlands",
+            "New Zealand",
+            "Nicaragua",
+            "Niger",
+            "Nigeria",
+            "Norway",
+            "Oman",
+            "Pakistan",
+            "Palau",
+            "Panama",
+            "Papua New Guinea",
+            "Paraguay",
+            "Peru",
+            "Philippines",
+            "Poland",
+            "Portugal",
+            "Qatar",
+            "Romania",
+            "Russian Federation",
+            "Rwanda",
+            "St Kitts & Nevis",
+            "St Lucia",
+            "Saint Vincent & the Grenadines",
+            "Samoa",
+            "San Marino",
+            "Sao Tome & Principe",
+            "Saudi Arabia",
+            "Senegal",
+            "Serbia",
+            "Seychelles",
+            "Sierra Leone",
+            "Singapore",
+            "Slovakia",
+            "Slovenia",
+            "Solomon Islands",
+            "Somalia",
+            "South Africa",
+            "South Sudan",
+            "Spain",
+            "Sri Lanka",
+            "Sudan",
+            "Suriname",
+            "Swaziland",
+            "Sweden",
+            "Switzerland",
+            "Syria",
+            "Taiwan",
+            "Tajikistan",
+            "Tanzania",
+            "Thailand",
+            "Togo",
+            "Tonga",
+            "Trinidad & Tobago",
+            "Tunisia",
+            "Turkey",
+            "Turkmenistan",
+            "Tuvalu",
+            "Uganda",
+            "Ukraine",
+            "United Arab Emirates",
+            "United Kingdom",
+            "United States",
+            "Uruguay",
+            "Uzbekistan",
+            "Vanuatu",
+            "Vatican City",
+            "Venezuela",
+            "Vietnam",
+            "Yemen",
+            "Zambia",
+            "Zimbabwe"});
+            this.cboxCountry.Location = new System.Drawing.Point(50, 46);
+            this.cboxCountry.Name = "cboxCountry";
+            this.cboxCountry.Size = new System.Drawing.Size(193, 24);
+            this.cboxCountry.TabIndex = 0;
+            this.cboxCountry.SelectedIndexChanged += new System.EventHandler(this.cboxCountry_SelectedIndexChanged);
+            // 
+            // cboxCity
+            // 
+            this.cboxCity.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboxCity.Enabled = false;
+            this.cboxCity.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cboxCity.FormattingEnabled = true;
+            this.cboxCity.Location = new System.Drawing.Point(259, 46);
+            this.cboxCity.Name = "cboxCity";
+            this.cboxCity.Size = new System.Drawing.Size(193, 24);
+            this.cboxCity.Sorted = true;
+            this.cboxCity.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(118, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Country";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(340, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "City";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 84);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(479, 23);
+            this.progressBar1.TabIndex = 3;
+            this.progressBar1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(503, 546);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cboxCity);
+            this.Controls.Add(this.cboxCountry);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Weather Forecast";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboxCountry;
+        private System.Windows.Forms.ComboBox cboxCity;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,82 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Windows.Forms;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace WeatherForecast
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<string> cityNames = new List<string>();
+
+        private async void cboxCountry_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cboxCity.Enabled = false;
+            progressBar1.Visible = true;
+            progressBar1.Style = ProgressBarStyle.Marquee;
+            cboxCity.Items.Clear();
+
+            string countryName = cboxCountry.Text;
+
+            RestRequest request = new RestRequest(Method.GET); //creating get RestRequest
+            request.AddHeader("apikey", "bxfF4nSrkpHHHjW8mWGThEP3IN5oNuWV"); //apikey for authentication
+
+            await Task.Run(()=> CountryIsoToCities(request, CountryNameToCountryIso(request, countryName))); //run methods for get cities
+
+            foreach (string item in cityNames) //add cities to combobox
+            {
+                if (!cboxCity.Items.Contains(item))
+                    cboxCity.Items.Add(item);
+            }
+
+            cboxCity.Enabled = true;
+            progressBar1.Visible = false;
+        }
+
+        private string CountryNameToCountryIso(RestRequest request,string countryName)
+        {
+            string countryIsoCode = "";
+
+            RestClient countryToIsoClient = new RestClient("https://api.apilayer.com/geo/country/name/" + countryName); //country name to
+                                                                                                                        //iso codes
+                                                                                                                        //restclient for api
+            dynamic cityIsoResponse = JsonConvert.DeserializeObject(countryToIsoClient.Execute(request).Content);
+            foreach (dynamic i in cityIsoResponse)
+            {
+                try
+                {
+                    countryIsoCode = i.alpha3code; //getting iso code by country
+                }
+                catch { }
+            }
+            return countryIsoCode;
+        }
+
+        private void CountryIsoToCities(RestRequest request,string countryIsoCode)
+        {
+            
+            string cityName = "";
+
+            RestClient isoToCityClient = new RestClient("https://api.apilayer.com/geo/country/cities/" + countryIsoCode); //iso code to cities
+                                                                                                                          //restclient for api
+            dynamic cityResponse = JsonConvert.DeserializeObject(isoToCityClient.Execute(request).Content); //getting and editing data
+            foreach (dynamic i in cityResponse) //selecting data required(city names)
+            {
+                try
+                {
+                    cityName = i.state_or_region; //getting cities by country iso code
+                    cityNames.Add(cityName);
+                }
+                catch { }
+            }
+        }
+    }
+}
